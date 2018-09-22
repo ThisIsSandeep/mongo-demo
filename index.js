2dcaab2936ed24301bf5a591b077e2f6bb0ef7cd
ThisIsSandeep <thesandybansal@gmail.com>
2018-09-22 15:16:02 +0530

    Course schema created

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ mongoose
     console.log('Connected to MongoDB...');
   })
   .catch(err => console.error('Could not connect to MongoDB..', err.message));
+
+// Schema (related to mongoose) Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection.
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
